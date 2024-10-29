ee2caa6435713b8f6c19ea265de6170a786f3a06
taramakhija51 <114837329+taramakhija51@users.noreply.github.com>
2024-10-29 23:28:16 +0000

    updated broken rcav, fixed currency formatting

--- app/controllers/super_controller.rb
+++ app/controllers/super_controller.rb
@@ -7,18 +7,19 @@ class SuperController < ApplicationController
   end
 
   def square_results
-    @num = params.fetch("url_keep").to_f
+    @num = params.fetch("num").to_f
     @square = @num * @num
     render({ :template => "layouts/square_results"})
   end
 
   def root_form
+    
     render({ :template => "layouts/root_form"})
   end
 
   def root_results
-    @find_root = params.fetch("find_root").to_f
-    @root = @find_root ** 0.5
+    @num = params.fetch("num").to_f
+    @root = @num ** 0.5
     render({ :template => "layouts/root_results"})
   end
 
@@ -27,17 +28,18 @@ class SuperController < ApplicationController
   end
 
   def payment_results
-    @apr = params.fetch("apr").to_f / 100
-    @num_years = params.fetch("num_years").to_f
+    @apr = params.fetch("APR").to_f / 100
+    @num_years = params.fetch("years_remaining").to_f
     @apr_monthly = @apr / 12
     @num_months = @num_years * 12
     @principal = params.fetch("principal").to_f
     @numerator = @principal * @apr_monthly
     @denominator = 1 - (1/((1 + @apr_monthly) ** (@num_months)))
-    @monthly = (@numerator / @denominator).round(2).to_s(:currency)
+    @monthly = (@numerator / @denominator).round(2)
+    @monthly = @monthly.to_fs(:currency)
     #@inner = ((((@apr / @num_months) + 1) ** @num_months) - 1)
     #@monthly = @principal * @inner
-
+    
     render({ :template => "layouts/payment_results"})
   end
 
@@ -47,8 +49,9 @@ class SuperController < ApplicationController
     
 
   def random_results
-    @minimum = params.fetch("min_num").to_f
-    @maximum = params.fetch("max_num").to_f
+    @minimum = params.fetch("user_min").to_f
+    @maximum = params.fetch("user_max").to_f
     @random = rand(@minimum..@maximum)
     render({ :template => "layouts/random_results"})
   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   get("/", {:controller => "super", :action => "homepage"})
-  get("/payment/form", {:controller => "super", :action => "payment_form"})
+  get("/payment/new", {:controller => "super", :action => "payment_form"})
   get("/payment/results", {:controller => "super", :action => "payment_results"})
-  get("/random/form", {:controller => "super", :action => "random_form"})
+  get("/random/new", {:controller => "super", :action => "random_form"})
   get("/random/results", {:controller => "super", :action => "random_results"})
-  get("/square/form", {:controller => "super", :action => "square_form"})
+  get("/square/new", {:controller => "super", :action => "square_form"})
   get("/square/results", {:controller => "super", :action => "square_results"})
-  get("/root/form", {:controller => "super", :action => "root_form"})
-  get("/root/results", {:controller => "super", :action => "root_results"})
+  get("/square_root/new", {:controller => "super", :action => "root_form"})
+  get("/square_root/results", {:controller => "super", :action => "root_results"})
 end
